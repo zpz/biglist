f8d5396206d252b8808757af39d8767dbe05a6d8
Zepu Zhang <zepu.zhang@gmail.com>
2021-08-14 16:43:34 -0700

    use serializer defined in upathlib

--- src/biglist/__init__.py
+++ src/biglist/__init__.py
@@ -1,12 +1,10 @@
 from ._biglist import Biglist, ListView, FileView, FileIterStat
-from ._serializer import Serializer
 
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 
 
 __all__ = [
     'Biglist', 'ListView', 'FileView',
-    'Serializer',
     'FileIterStat',
 ]

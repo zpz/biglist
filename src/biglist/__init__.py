f89eeb9b51f8aa72a48881e3734b85661f24ce94
zpz <zepu.zhang@gmail.com>
2022-11-04 21:02:14 -0700

    minor

--- src/biglist/__init__.py
+++ src/biglist/__init__.py
@@ -1,4 +1,8 @@
 from ._biglist import Biglist, ListView, FileView
+try:
+    from ._parquet import ParquetList
+except ImportError:
+    ParquetList = None
 
 
 __version__ = "0.6.9"
@@ -9,3 +13,6 @@ __all__ = [
     "ListView",
     "FileView",
 ]
+
+if ParquetList is not None:
+    __all__.append('ParquetList')
